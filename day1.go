e4b95bf49895e67ea416576a31ded8ba507f0cd8
Mykyta Ovsiienko <68018445+movsiienko@users.noreply.github.com>
2025-12-01 23:28:06 +0200

    part 2

--- day1.go
+++ day1.go
@@ -11,40 +11,45 @@ const MAX_ITEMS = 100
 
 func main() {
 	data, err := os.ReadFile("input.txt")
-	input := strings.Split(string(data), "\n")
 	if err != nil {
-		fmt.Errorf("Failed to read the file: %s", err)
+		fmt.Printf("Failed to read the file: %s", err)
 		panic(1)
 	}
+	input := strings.Split(string(data), "\n")
 	currentValue := 50
 	code := 0
-	for _, v := range input {
+	for i, v := range input {
 		if v == "" {
 			continue
 		}
 		direction := string(v[0])
 		valueStr := v[1:]
 		value, _ := strconv.Atoi(valueStr)
-		value = value % 100
+		iterations := value / MAX_ITEMS
+		value = value % MAX_ITEMS
 		switch direction {
 		case "L":
+			if currentValue == 0 {
+				currentValue = 100
+			}
 			currentValue -= value
 		case "R":
 			currentValue += value
-
 		}
-		// fmt.Printf("Interminidate value: %d . ", currentValue)
 		if currentValue < 0 {
+			iterations += (currentValue / MAX_ITEMS) + 1
 			currentValue += MAX_ITEMS
 		} else if currentValue > MAX_ITEMS {
+			iterations += currentValue / MAX_ITEMS
 			currentValue -= MAX_ITEMS
 		} else if currentValue == MAX_ITEMS {
 			currentValue = 0
+			iterations += 1
+		} else if currentValue == 0 {
+			iterations += 1
 		}
-		// fmt.Printf("Index: %d, Input was: %s, currentValue: %d \n", i, v, currentValue)
-		if currentValue == 0 {
-			code += 1
-		}
+		code += iterations
+		fmt.Printf("Index: %d, Input was: %s, currentValue: %d, code: %d, value: %d, iterations: %d \n", i, v, currentValue, code, value, iterations)
 
 	}
 	fmt.Println(code)
