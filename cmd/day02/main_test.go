8881591e9eff77764670952845364a61c9415f01
Mykyta Ovsiienko <68018445+movsiienko@users.noreply.github.com>
2025-12-08 12:36:56 +0200

    test: more tests

--- cmd/day02/main_test.go
+++ cmd/day02/main_test.go
@@ -2,38 +2,40 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"testing"
 )
 
-func TestIsValidId(t *testing.T) {
+func TestIsValidIdRanges(t *testing.T) {
+	count := 0
 	var tests = []struct {
-		number int
-		want   bool
+		startRange int
+		endRange   int
+		invalid    []int
 	}{
-		// {222222, false},
-		// {565656, false},
-		// {1010, false},
-		// {11, false},
-		// {22, false},
-		// {99, false},
-		// {111, false},
-		// {999, false},
-		// {1010, false},
-		{1188511885, false},
-		// {222222, false},
-		{446446, false},
-		// {38593859, false},
-		// {565656, false},
-		// {824824824, false},
-		// {2121212121, false},
+		{11, 22, []int{11, 22}},
+		{95, 115, []int{99, 111}},
+		{998, 1012, []int{999, 1010}},
+		{1188511880, 1188511890, []int{1188511885}},
+		{222220, 222224, []int{222222}},
+		{1698522, 1698528, []int{}},
+		{446443, 446449, []int{446446}},
+		{38593856, 38593862, []int{38593859}},
+		{565653, 565659, []int{565656}},
+		{824824821, 824824827, []int{824824824}},
+		{2121212118, 2121212124, []int{2121212121}},
 	}
 	for _, tt := range tests {
-		testname := fmt.Sprintf("%d", tt.number)
-		t.Run(testname, func(t *testing.T) {
-			isValid := isValidId(tt.number)
-			if isValid != tt.want {
-				t.Errorf("got %t, want %t", isValid, tt.want)
-			}
-		})
+		for i := tt.startRange; i <= tt.endRange; i++ {
+			testName := fmt.Sprintf("%d", i)
+			t.Run(testName, func(t *testing.T) {
+				isValid := isValidId(i)
+				count++
+				expected := !slices.Contains(tt.invalid, i)
+				if isValid != expected {
+					t.Errorf("Got %t, want %t", isValid, expected)
+				}
+			})
+		}
 	}
 }
